46b717897552437fc9fdf2c0240c184bdda3bae0
Aaron Wilson <codingsensei19@gmail.com>
2020-02-01 13:59:23 +0000

    Automatically backed up by Learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,17 +1,17 @@
 def oxford_comma(array)
-  if array.length == 1 
+  if array.length == 1
   array.join('')
-  
-  elsif array.length == 2 
+
+  elsif array.length == 2
     array.join(' and ')
-    elsif array.length >= 3 
+    elsif array.length >= 3
       array[-1] = "and #{array[-1]}"
       array.join(', ')
-    else 
-      nil 
-    end 
-end   
-  
+    else
+      nil
+    end
+end
+
 oxford_comma(["kiwi"])
 oxford_comma(["kiwi", "durian"])
 oxford_comma(["kiwi", "durian", "starfruit"])
